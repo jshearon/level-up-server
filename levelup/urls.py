728f98de7e46e17b395aa7a04d94591820f9b607
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-11-21 11:25:08 -0600

    created game-types view

--- levelup/urls.py
+++ levelup/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import include
 from django.urls import path
-from levelupapi.views import register_user, login_user
+from levelupapi.views import register_user, login_user, GameTypesViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypesViewSet, 'gametype')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
