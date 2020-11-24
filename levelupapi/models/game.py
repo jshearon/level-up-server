fa6ba95f2daa91811b2293f7e86f80563fe4d91f
Jonathan Shearon <jonathan.shearon@gmail.com>
2020-11-23 18:54:31 -0600

    fixed db schema in games model

--- levelupapi/models/game.py
+++ levelupapi/models/game.py
@@ -2,13 +2,15 @@ from django.db import models
 from django.db.models.deletion import CASCADE
 
 class Game(models.Model):
-  name = models.CharField(max_length=50)
+  title = models.CharField(max_length=75, default=None)
+  maker = models.CharField(max_length=75, default=None)
   gamer = models.ForeignKey("Gamer", 
     on_delete=CASCADE,
     related_name="games",
     related_query_name="game")
-  game_type = models.ForeignKey("GameType",
+  gametype = models.ForeignKey("GameType",
     on_delete=CASCADE,
     related_name="games",
     related_query_name="game")
-  max_players = models.IntegerField()
+  number_of_players = models.IntegerField()
+  skill_level = models.IntegerField(default=0)
