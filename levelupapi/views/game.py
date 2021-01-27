47a77d096ecc27bb451063dde2ab1c362597afc8
Jonathan Shearon <jonathan.shearon@gmail.com>
2021-01-26 21:14:49 -0600

    wip

--- levelupapi/views/game.py
+++ levelupapi/views/game.py
@@ -28,14 +28,14 @@ class Games(ViewSet):
         game = Game()
         game.title = request.data["title"]
         game.maker = request.data["maker"]
-        game.number_of_players = request.data["numberOfPlayers"]
-        game.skill_level = request.data["skillLevel"]
+        game.number_of_players = request.data["number_of_players"]
+        game.skill_level = request.data["skill_level"]
         game.gamer = gamer
 
         # Use the Django ORM to get the record from the database
         # whose `id` is what the client passed as the
         # `gameTypeId` in the body of the request.
-        gametype = GameType.objects.get(pk=request.data["gameTypeId"])
+        gametype = GameType.objects.get(pk=request.data["gametype"])
         game.gametype = gametype
 
         # Try to save the new game to the database, then
@@ -44,7 +44,7 @@ class Games(ViewSet):
         try:
             game.save()
             serializer = GameSerializer(game, context={'request': request})
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         # If anything went wrong, catch the exception and
         # send a response with a 400 status code to tell the
