c356a5bf3cd26a968aaf8df8376572efc9fddc14
Jonathan Shearon <jonathan.shearon@gmail.com>
2021-02-06 10:35:46 -0600

    wip

--- levelup/settings.py
+++ levelup/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,9 +142,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-import os
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
-import django_on_heroku
 django_on_heroku.settings(locals())
